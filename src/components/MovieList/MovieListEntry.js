60d90445e7d10316be4529ffdeb1a124f9f494b9
Simon Bächler <simon@stellanera.com>
2018-01-21 11:05:08 +0100

    Add posters

--- src/components/MovieList/MovieListEntry.js
+++ src/components/MovieList/MovieListEntry.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { POSTER_BASE_URL, POSTER_SIZES } from '../../config/constants';
 
 export default function MovieListEntry(props) {
 
   const movie = props.movie;
+  const posterPath = `${POSTER_BASE_URL}/${POSTER_SIZES['342']}/${movie.poster_path}`;
 
   return(
-      <div className="movie-list__entry">{movie.title}</div>
+      <div className="movie-list__entry card">
+        <img src={posterPath} alt="" />
+        <div className="card-section">
+          <h2 className="h4">{movie.title}</h2>
+        </div>
+      </div>
   );
 }
